ff1efef42e9921630b0c036d37cb69a5451b11e8
Akramobt <39308624+Akramobt@users.noreply.github.com>
2022-07-29 15:54:23 +0200

    Finishing Home Page

--- src/components/Excercises.js
+++ src/components/Excercises.js
@@ -18,6 +18,21 @@ const Excersises = ({excercises,setExercises,bodyPart}) => {
     window.scrollTo({top:1800,behavior:'smooth'})
 
   }
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
 
   return (
     <Box id="excercises"
